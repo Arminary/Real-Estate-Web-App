51f77ac33f295a85c6845411305d2b964ba57298
Arminary <arimoroadeifeoluwa@gmail.com>
2024-02-22 13:06:51 +0100

    Create Sign In Api Route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcrypt';
-// import { errorHandler } from "../utils/error.js";
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
    try{
@@ -21,3 +23,19 @@ export const signup = async (req, res, next) => {
    }
     
 };
+
+export const signin = async (req, res, next) => {
+   const { email, password } = req.body;
+   try {
+      const validUser = await User.findOne({ email });
+      if(!validUser) return next(errorHandler('User not Found', 404));
+      const validPassword = await bcrypt.compare(password, validUser.password);
+      if(!validPassword) return next(errorHandler('invalid credentials!', 401));
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+      const { password: pass, ...rest } = validUser._doc;
+      res.cookie('access_token', token, { httpOnly: true });
+      res.status(200).json({ rest });
+   } catch (error) {
+      next(error);
+   }
+}
