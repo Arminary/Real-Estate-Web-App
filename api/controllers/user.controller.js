535d033c63b1bf7601b4434bf525917e77c1d4b5
Arminary <arimoroadeifeoluwa@gmail.com>
2024-02-27 11:00:50 +0100

    add delete function

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -31,3 +31,14 @@ export const updateUser = async (req, res, next) => {
         
     }
 }
+
+export const deleteUser = async (req, res, next) =>{
+    if(req.user.id !== req.params.id) return next(errorHandler(401, 'you can only delete your own account'))
+    try {
+       await User.findByIdAndDelete(req.params.id)
+       res.status(200).json('User has been deleted...')
+       res.clearCookie9('access_token');
+    } catch (error) {
+       next(error) 
+    }
+}
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { useRef, useState, useEffect } from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserStart, updateUserSuccess, updateUserFailure } from '../redux/user/userSlice.js';
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice.js';
 import { useDispatch } from 'react-redux';
 
 export default function Profile() {
@@ -84,6 +84,23 @@ export default function Profile() {
     } catch (error) {
       dispatch(updateUserFailure(error.message));
     }
+  };
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
   }
 
   return (
@@ -117,7 +134,7 @@ export default function Profile() {
         <button onClick={handleSubmit} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-80 disabled:opacity-50'>{loading ? "Loading..." : "Update"}</button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete Account</span>
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
         <span className='text-red-700 cursor-pointer'>Sign Out</span>
 
       </div>
